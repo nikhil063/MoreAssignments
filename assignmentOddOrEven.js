bb334b7dee5d3ad6bd1f837b5aa603af177c6e83
nikhil063 <122106278+nikhil063@users.noreply.github.com>
2023-02-02 19:11:50 +0530

    Update assignmentOddOrEven.js

--- assignmentOddOrEven.js
+++ assignmentOddOrEven.js
@@ -1,6 +1,8 @@
-var age = 19;
-if(age>18){
-console.log("Yes");
+// JavaScript program to check whether a number is even or not
+let num = prompt("Enter a number: ");
+if(num%2 == 0){
+ console.log("your number is even");   
 }
-else
-{console.log("No");}
+else{
+    console.log("your number is odd");  
+}
